0178688a57fcc48f16fd70add554f3de40a64e99
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-02-16 08:46:42 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * printf - Writes its argument on standard output i.e. the output stream
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
