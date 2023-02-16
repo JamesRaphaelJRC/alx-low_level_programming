70d49e5b5dcef7fe6d46ec281ef4a467344027b2
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-02-16 12:18:59 -0800

    Unix genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 /**
  *main -  Prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
+ *
+ * Return: 1
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(str)-1, 1, stderr);
+	fwrite(str, sizeof(str) - 1, 1, stderr);
 	return (1);
 }
