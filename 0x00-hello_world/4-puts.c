3ca73d1dc7ecf07e7fca64554d976d648cc3a2e8
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-02-16 08:37:58 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * puts writes argument in char 'str' to the output stream.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
