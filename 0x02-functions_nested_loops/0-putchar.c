d8d555e6b31615c861bbe54ce810c5078de62227
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-02-21 05:15:24 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" using our _putchar function in our
+ *		header-file main.h
+ *
+ * Return: Always 0 (Successful).
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
