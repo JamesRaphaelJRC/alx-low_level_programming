b1766cd57ab9a3bce8d0a20289eaf8feb40cf03e
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-02-21 20:50:34 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers and return the result
+ * @x: The first argument.
+ * @y: The second argument
+ *
+ * Return: The sum of x and y.
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
