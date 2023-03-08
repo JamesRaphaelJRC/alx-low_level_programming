d3727778af9b45ada284680ad7e4288d1132b968
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-08 09:07:23 -0800

    strlen recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  *
  * Return: An integer equivalent to the length of the supplied string.
  */
+int i = 0;
+
 int _strlen_recursion(char *s)
 {
-	static int i;
-
 	if (*s)
 	{
 		_strlen_recursion(s + 1);
