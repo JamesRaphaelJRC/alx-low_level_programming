9d3833aa041294f3115ab0e1646dd0c15e040e7e
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-10 09:54:16 -0800

    adding command line args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,20 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (isdigit(*argv[i]) && *argv[i] > 0)
+		for (j = 0; argv[i][j]; j++)
 		{
-			sum += atoi(argv[i]);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
