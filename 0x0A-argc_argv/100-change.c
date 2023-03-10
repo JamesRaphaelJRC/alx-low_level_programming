f0f4657880fa0dee0658649e753f9b5871fbfc32
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-10 12:20:43 -0800

    How many pieces of change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the minimum number of coins to make change for an
+ *		amount of money.
+ * @argc: Number of command line arguments.
+ * @argv: An array of pointers to the argument(s).
+ *
+ * Return: 1 when more than one argument is passed to the function, 0 success.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+
+	printf("%d\n", coins);
+	return (0);
+}
