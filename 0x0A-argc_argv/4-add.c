2adf87cbf904befc008f8f7c2ad146710dd489e2
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-12 03:41:01 -0700

    main version

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum = 0;
 
-	if (argc > 2)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
-			{
-				sum += atoi(argv[i]);
-			}
-			else
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
