27070c0ebced1920506c611e1e557b2f3db490f2
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-13 08:38:15 -0700

    Task 2: _stdrup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 
 	while (str[len] != '\0')
 		len++;
-	new_str = malloc(len * sizeof(char));
+	new_str = malloc(len * sizeof(char) + 1);
 
 	if (new_str == NULL)
 		return (NULL);
