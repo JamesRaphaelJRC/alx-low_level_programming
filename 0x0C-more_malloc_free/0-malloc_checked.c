35a6a40e28d5f868a9c7715470464dbfc7be7474
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-16 03:14:17 -0700

    Task 0: malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: An unsigned int indicating the number of elements.
+ *
+ * Return: A pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b * sizeof(ptr));
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains my ALX-SE tasks on more malloc and free (Dynamic memory allocation)
