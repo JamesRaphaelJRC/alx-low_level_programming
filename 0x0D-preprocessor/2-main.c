0db55107ddeb72fed5993bd2645c0faff882b253
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-21 06:22:20 -0700

    Task 2: print filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from,
+ *		followed by a new.
+ *
+ * Return - 0 on succes.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
