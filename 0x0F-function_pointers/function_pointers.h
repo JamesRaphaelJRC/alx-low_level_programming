f1c24d3ce52a6f4c60d9355f47321b18ef5e4eac
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-22 07:58:11 -0700

    Task 0: Print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,12 @@
 #ifndef __FUNCTION_POINTERS_H__
 #define __FUNCTION_POINTERS_H__
+#include <stdlib.h>
 
+/*
+ * File: function_pointers.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
