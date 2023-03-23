ff508b5aa403da9afd7c3c726b02c33dafdb0fbb
JamesRaphaelJRC <Jamesraphaeljrc@gmail.com>
2023-03-23 06:09:52 -0700

    Task 1 fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i, j = 0;
 
-	if (n == 0)
-		return;
-
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
