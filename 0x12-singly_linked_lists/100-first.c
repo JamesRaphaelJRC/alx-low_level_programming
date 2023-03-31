57232d9df20235a59d4b16df2c9810f8bee20d4e
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-03-31 05:06:30 -0700

    task 100 bugfix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,7 @@
  * print_first - Prints a string before main function is executed.
  * Return: void
  */
-void print_first(void)__attribute__((constructor));
-void print_first(void)
+void __attribute__((constructor)) print_first(void)
 {
 	printf("You're beat! and yet, you must allow,
 			\nI bore my house upon my back!\n");
