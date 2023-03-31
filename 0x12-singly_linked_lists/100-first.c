c0014e1ddb97ec67e9105dd7c7f88755a7f3c97b
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-03-31 05:13:17 -0700

    Task 100 bugfix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__((constructor)) print_first(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
