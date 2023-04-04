5e8fc55001687d640b51e6b6d7919c592e977980
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-04 08:58:20 -0700

    Task 9 bugfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,57 +4,37 @@
 /**
  * insert_nodeint_at_index - Inserts a node at a  give position.
  * @head: A listint_t pointer to the listint_t linked list.
- * @idx: The index where node should be inserted.
  * @n: The data to initialize the newnode to be inserted.
  *
- * Return: A pointer to the newly inserted node.
+ * @Return: A pointer to the newly inserted node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp = *head;
-	listint_t *newnode;
-	unsigned int count = 0;
+	listint_t *new, *tmp = *head;
+	unsigned int node;
 
-	if (*head == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
-	while (tmp)
-	{
-		count++;
-		tmp = tmp->next;
-	}
-	if (idx > count)
-		return (NULL);
-
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-		return (NULL);
-
-	tmp = *head;
-	count = 1;
-	newnode->n = n;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
-	}
-	if (*head == NULL)
-	{
-		newnode->next = NULL;
-		*head = newnode;
+		new->next = tmp;
+		*head = new;
 	}
 	else
 	{
-		while (count < idx)
+		for (node = 1; node < idx; node++)
 		{
-			tmp = tmp->next;
-			count++;
-		}
-		newnode->next = tmp->next;
-		tmp->next = newnode;
-	}
+			if (tmp == NULL || tmp->next == NULL)
+				return (NULL);
 
-	return (newnode);
+			tmp = tmp->next;
+		}
+		new->next = tmp->next;
+		tmp->next = new;
+	}
+	return (new);
 }
