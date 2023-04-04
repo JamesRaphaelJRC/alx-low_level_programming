1a0516feac8880e6d8aeaf0137c6cdb4b637e77d
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-04 05:26:14 -0700

    Task 7 fixed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
 	tmp = head;
 	while (count < index)
