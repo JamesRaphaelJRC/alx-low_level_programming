fdab093f7a9f2f454a8dc5536151bdfa729ab18e
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-04 11:23:47 -0700

    Redone

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *nextnode, *tmp = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	if (index == 0)
