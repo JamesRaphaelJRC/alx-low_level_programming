0dae19bcaa91c1e1a132d87e61019103b59a1067
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-05 07:04:22 -0700

    Task 101 fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,6 +13,8 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *current;
 	size_t count = 0;
 
+	if (head == NULL)
+		exit (98);
 	while (head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
