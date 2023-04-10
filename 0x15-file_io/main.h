cc970f690adeaa270f40c6fa46c41b54e05d1f71
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-10 08:22:36 -0700

    task 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
-#include <stddef.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
