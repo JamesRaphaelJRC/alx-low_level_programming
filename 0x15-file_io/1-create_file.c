75799025d860e8415c10d2b9d49f3aa2a6b8bd42
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-10 08:35:34 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, count);
 
-	if (fd == -1 || w == -1 || w != count)
+	if (fd == -1 || w == -1)
 		return (-1);
 
 	close(fd);
