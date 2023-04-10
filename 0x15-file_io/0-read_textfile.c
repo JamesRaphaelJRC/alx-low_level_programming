cbb1d8e1b082e33be1b7464cbdf5982804517372
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-10 07:06:48 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a test file and prints it to the POSIX standard output
+ * @filename: File to be read and printed.
+ * @letters: Number of letters(bytes) to read and print.
+ *
+ * Return: 0 if filename is NULL or if file cannot be opened or read
+ *		The actual number of letters it could read or print on success.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t num, fd, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	num = read(fd, buffer, letters);
+	w = write(1, buffer, num);
+
+	if (fd == -1 || num == -1 || w != num)
+		return (0);
+
+	else
+		return (num);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
