ee497db835734628e67baa8bc9fca2539f802b99
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-04-11 05:46:33 -0700

    Task 3 fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,53 +4,88 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
 /**
  * main - Copies the content of a file to another file.
- * @argc: Number of commandline arguments.
- * @argv: Commandline arguments.
+ * @argc: Number of command line arguments.
+ * @argv: Command line arguments.
  *
  * Return: 0 on success, other exit values otherwise.
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2, r, w, c1, c2;
-	char *buffer, *f_from, *f_to;
+	int fd1, fd2, r, w;
+	char *buffer;
+	struct stat st;
 
 	if (argc != 3 || argv == NULL)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	f_from = argv[1];
-	f_to = argv[2];
+
+	if (stat(argv[1], &st) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		exit(98);
+	}
 
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
-		exit(98);
-
-	fd1 = open(f_from, O_RDONLY);
-	r = read(fd1, buffer, 1024);
-	if (fd1 == -1 || r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", f_from);
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		exit(98);
+	}
+
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		free(buffer);
 		exit(98);
 	}
-	c1 = close(fd1);
-	fd2 = open(f_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	w = write(fd2, buffer, r);
-	if (fd2 == -1 || w == -1)
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, st.st_mode & 0666);
+	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buffer);
 		exit(99);
 	}
-	c2 = close(fd2);
-	if (c1 == -1 || c2 == -1)
+
+	while ((r = read(fd1, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		w = write(fd2, buffer, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		free(buffer);
 		exit(100);
 	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		free(buffer);
+		exit(100);
+	}
+
+	free(buffer);
 	return (0);
 }
