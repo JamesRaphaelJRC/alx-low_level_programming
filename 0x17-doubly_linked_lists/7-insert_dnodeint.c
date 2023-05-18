1aec304a8ec7a5e1f6b8bc578f7037c2df84947c
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-18 13:10:28 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	tmp = *h;
 
-	if (idx > node_len || idx)
+	if (idx > node_len)
 		return (NULL);
 
 	if (idx == 0 || *h == NULL)
