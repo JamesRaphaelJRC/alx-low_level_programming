d7bcc1745d10077c664129b7104e3eb448e92e7c
JamesRaphelJRC <jamesraphaeljrc@gmail.com>
2023-05-22 11:58:34 -0700

    Task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - add two numbers
+ * @a: num
+ * @b: num
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: num
+ * @b: num
+ *
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: num
+ * @b: num
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two numbers
+ * @a: num
+ * @b: num
+ *
+ * Return: int
+ */
+float div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - find modulo of two numbers
+ * @a: num
+ * @b: num
+ *
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
