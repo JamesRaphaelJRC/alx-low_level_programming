365d16a01a564783d94961506328acbad40b779a
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-09 19:40:58 -0700

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <string.h>
 
 /**
  * hash_table_get - Retrieve the value associated with
@@ -11,7 +12,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
+	hash_newnode_t *newnode;
 	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
@@ -21,9 +22,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
+	newnode = ht->array[index];
+	while (newnode && strcmp(newnode->key, key) != 0)
+		newnode = newnode->next;
 
-	return ((node == NULL) ? NULL : node->value);
+	return ((newnode == NULL) ? NULL : newnode->value);
 }
