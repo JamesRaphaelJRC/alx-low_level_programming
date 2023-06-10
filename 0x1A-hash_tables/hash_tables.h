0d28a3346f59215dc8944884b4859974041a6595
JamesRaphaelJRC <jamesraphaeljrc@gmail.com>
2023-06-09 19:53:09 -0700

    Task 4

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,7 +1,9 @@
 #ifndef __HASH_TABLES_H__
 #define __HASH_TABLES_H__
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /**
@@ -39,6 +41,7 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 
 #endif
